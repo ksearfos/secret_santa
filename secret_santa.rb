ab353b8a0bbd6175db1232cef14134e85a31c0ac
ksearfos <ksearfos@covermymeds.com>
2019-10-25 13:12:37 -0400

    update phrasing in script to eliminate ambiguity

--- secret_santa.rb
+++ secret_santa.rb
@@ -4,11 +4,11 @@ require_relative "lib/secret_santa"
 
 PEOPLE = [
   %w(Kelli Terri Bryan Andrea Eric),
-  %w(Liz Dave Mike Linda Ben Emily),
+  ['Liz', 'Dave', 'Uncle Mike', 'Linda', 'Ben', 'Emily'),
   'Grandma',
-  "Kelli's +1",
+  "Kelli's Friend Mike",
 ]
 
 SecretSanta.new(PEOPLE).assignments.each do |santa,assignee|
-  puts "#{santa} is assigned to #{assignee}"
+  puts "#{santa} is getting a gift for #{assignee}"
 end
